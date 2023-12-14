16ae6dbd7dac36f7f351c88d1f97cda121ad25b4
ebel-frank <frankcebeledike@gmail.com>
2023-12-14 17:14:53 +0100

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoaEasyPay.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase {
+        public readonly IConfiguration config;
+        public AuthController(IConfiguration config)
+        {
+            this.config = config;
+        }
+    }
+}
